485b249dfbc690beb59a34bab80efe50ab10d605
Gabecker977 <88011617+Gabecker977@users.noreply.github.com>
2022-08-26 13:31:07 -0300

    Update PickupObjects.cs

--- Padrões Criacionais/Abstract Factory/Exemplo no Unity/PickupObjects.cs
+++ Padrões Criacionais/Abstract Factory/Exemplo no Unity/PickupObjects.cs
@@ -7,14 +7,11 @@ public class PickupObjects : Interactable
 {   [SerializeField] private Transform parent;
     [SerializeField] private Image image;
     //Ao olhar para o objeto mostrar imagem de interação
-    public override void OnFocus()
-    {
+    public override void OnFocus(){
         image.gameObject.SetActive(true);
     }
     //Ao interagir com o objeto "pegar" ele
-    public override void OnInteract()
-    {
-    
+    public override void OnInteract(){
         gameObject.transform.parent=parent;
         gameObject.GetComponent<Rigidbody>().useGravity=false;
         gameObject.GetComponent<Rigidbody>().drag=10;
@@ -23,17 +20,14 @@ public class PickupObjects : Interactable
          //gameObject.GetComponent<Rigidbody>().AddForce(gameObject.transform.position-transform.forward,ForceMode.Impulse);
     }
     //Ao parar de interagir com o objeto "soltar" ele
-    public override void StopInteract()
-    {
-        
+    public override void StopInteract(){
         gameObject.transform.parent=null;
         gameObject.GetComponent<Rigidbody>().useGravity=true;
         gameObject.GetComponent<Rigidbody>().drag=1;
         //gameObject.GetComponent<Rigidbody>().AddExplosionForce(500f,parent.transform.forward,4f);
     }
     //Ao para de olhar para o objeto cancelar a interação e remover imagem de interação
-    public override void OnLoseFocus()
-    {
+    public override void OnLoseFocus(){
         gameObject.transform.parent=null;
         gameObject.GetComponent<Rigidbody>().useGravity=true;
         gameObject.GetComponent<Rigidbody>().drag=1;
